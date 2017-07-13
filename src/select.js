cc26a2c6c78914fb621ea05bbce28c262386f06b
john fritz <johnfritz@johns-Mac-mini.local>
2017-07-12 21:44:01 -0700

    update

--- src/select.js
+++ src/select.js
@@ -1,21 +1,21 @@
 /*eslint no-unused-vars: 0*/
 'use strict';
 
-import FabricCanvasTool from './fabrictool'
+import FabricCanvasTool from './fabrictool';
 
 class Select extends FabricCanvasTool {
-
-    configureCanvas(props) {
-        let canvas = this._canvas;
-        canvas.isDrawingMode = false;
-        canvas.selection = true;
-        canvas.forEachObject((o) => {
-            console.log(o)
-            o.selectable = o.evented = true;
-        });
-        const background = canvas.item(0);
-        background.selectable = background.evented = false; // first (background) image not selectable
-    }
+  configureCanvas(props) {
+    let canvas = this._canvas;
+    canvas.isDrawingMode = false;
+    canvas.selection = true;
+    canvas.forEachObject((o, i) => {
+      if (i !== 0) {
+        o.selectable = o.evented = true;
+      }
+    });
+   // const background = canvas.item(0);
+   // background.selectable = background.evented = false; // first (background) image not selectable
+  }
 }
 
 export default Select;
